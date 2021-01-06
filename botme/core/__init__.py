6347f7da775607ab06ea8ff422858124fe7ddc37
Jordi Irawan <64052045+jordirwn@users.noreply.github.com>
2021-01-06 15:52:06 +0700

    Update __init__.py

--- botme/core/__init__.py
+++ botme/core/__init__.py
@@ -4,10 +4,10 @@ import glob
 from botme import logger
 
 
-def modules():
+def get_modules():
     for i in glob.glob("*/*/*.py"):
         if not i.endswith("__init__.py"):
             yield re.sub(r".+/|.py", "", i)
 
-modules = list(tuple(modules()))
+modules = list(tuple(get_modules()))
 logger.info(f"Berhasil menghidupkan {len(modules)} modul : {modules}")
